cd03c8eb004398b4a30b99fd022b4d4f905095bf
Jace <xjairtorres07@gmail.com>
2025-09-26 15:32:26 -0500

    actualización del header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,21 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cuantica Group</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+</head>
+
+<body>
+
+    <header></header>
+
+
     <div class="flex min-h-screen bg-gray-50">
         <!-- Lado Izquierdo -->
         <div class="flex w-full md:w-1/2 items-center justify-center p-8">
@@ -130,4 +147,9 @@
         </div>
 
     </div>
-</x-app-layout>
+
+    <footer></footer>
+
+</body>
+
+</html>
